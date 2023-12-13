0cfc4ce8f79bfbe2d9f00d2f09bca3662da93d41
owisg <owisgamal.2020@gmail.com>
2023-12-13 02:26:43 -0800

    4a

---
+++ rotr.c
@@ -0,0 +1,31 @@
+include "monty.h"
+
+/**
+ * f_rotr - rotates the stack to the bottom
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *temp, *last;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+
+	temp = *head;
+	last = *head;
+
+	while (last->next)
+	{
+		last = last->next;
+	}
+
+	last->prev->next = NULL;
+	last->prev = NULL;
+	last->next = *head;
+	(*head)->prev = last;
+	*head = last;
+}
---
+++ stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void f_stack(stack_t **head, unsigned int counter)
+{
+    (void)head;
+    (void)counter;
+    
+    // Alternative implementation with the same output
+    bus.lifi = 0;
+
+}
