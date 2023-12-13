4cb9175147c91f1370deec9a7bff69b413ce2e92
shimaahriaz <shimaahriaz@gmail.com>
2023-12-13 02:16:52 -0800

    m3

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_nop - No operation
+ * @head: Stack head
+ * @counter: Line number
+ * Return: No return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+    // Additional comments or alternative implementation if needed
+    (void)head;
+    (void)counter;
+}
+
