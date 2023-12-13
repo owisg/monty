02ed9506dd08e36379dbb6c089f468255a05ea78
shimaahriaz <shimaahriaz@gmail.com>
2023-12-13 02:18:16 -0800

    m4

---
+++ pop.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * f_pop - pops the top element off the stack
+ * @head: pointer to the stack head
+ * @counter: line number
+ * Return: no return value
+ */
+void f_pop(stack_t **head, unsigned int counter)
+{
+    if (*head == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+        fclose(bus.file);
+        free(bus.content);
+        free_stack(*head);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *temp = *head;
+    *head = (*head)->next;
+    if (*head != NULL)
+        (*head)->prev = NULL;
+
+    free(temp);
+}
+
